89ee90f61cca25fbf7531a48b16b14284a9f7cf2
AntonioNeto0 <antonioaraujo99@gmail.com>
2023-06-11 17:50:28 -0300

    Mapa de distribuiução das mortes e histograma com a quantidade de mortes por etnia

--- Analise_Graficos/analise_1.py
+++ Analise_Graficos/analise_1.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -32,3 +33,44 @@ plt.tight_layout()
 
 
 st.pyplot(fig)
+
+
+#Carrega a base de dados fatal_encounters_dot_org
+dataframe_fatal_encounters = pd.read_csv('../data/fatal_encounters_dot_org.csv')
+
+#Ajustar colunas
+dataframe_fatal_encounters.rename(columns={'Latitude': 'LATITUDE'}, inplace=True)
+dataframe_fatal_encounters.rename(columns={'Longitude': 'LONGITUDE'}, inplace=True)
+dataframe_fatal_encounters['LATITUDE'] = dataframe_fatal_encounters['LATITUDE'].astype(float)
+dataframe_fatal_encounters['LONGITUDE'] = dataframe_fatal_encounters['LONGITUDE'].astype(float)
+dataframe_fatal_encounters['LATITUDE'].fillna(0, inplace=True)
+dataframe_fatal_encounters['LONGITUDE'].fillna(0, inplace=True)
+
+dataframe_fatal_encounters["Subject's race"].fillna("Race unspecified", inplace=True)
+
+
+#Plotagem do mapa
+selected_race = st.selectbox('Selecione uma opção', dataframe_fatal_encounters["Subject's race"].unique())
+
+st.map(dataframe_fatal_encounters[dataframe_fatal_encounters["Subject's race"] == selected_race])
+
+
+#Plotagem do histograma
+# Filtra o dataframe para uma raça específica
+causa_especifica = st.selectbox('Selecione uma opção', dataframe_fatal_encounters["Cause of death"].unique())
+st.subheader('Valor absoluto de mortes separado por etnia')
+df_filtrado = dataframe_fatal_encounters[dataframe_fatal_encounters["Cause of death"] == causa_especifica]
+
+contagem = df_filtrado["Subject's race"].value_counts()
+
+st.bar_chart(contagem)
+
+contagem.plot(kind='bar')
+
+# Configurações do gráfico
+plt.xlabel('Causa da Morte')
+plt.ylabel('Contagem')
+plt.title(f'Valor absoluto separado por raça de mortes por: {causa_especifica}')
+
+# Exibe o gráfico
+st.pyplot(plt)
