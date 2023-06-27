511c71f05ee86b68d3f454fb5fd35e029059041e
Caio Mizan <caiomizan1@gmail.com>
2023-06-27 01:39:56 -0300

    main.py e exibição dos dataset

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title = "Police Shooting",
+    layout = "wide")
+
+def carregar_dataset():
+    
+    with st.expander('PoliceKillings'):
+        dataPoliceKillings = pd.read_csv("data\police_killings_MPV.csv")
+        st.write(dataPoliceKillings)
+    with st.expander('ShareRaceByCity'):
+        dataShareRaceByCity= pd.read_csv("data\ShareRaceByCity.csv")
+        st.write(dataShareRaceByCity)
+    with st.expander('ShootingsWash'):
+        shootingsWash = pd.read_csv("data\shootings_wash_post.csv")
+        st.write(shootingsWash)
+
+def barra_Lateral():
+    opcoes = ["Informação 1", "Informação 2"]
+    selecao = st.sidebar.selectbox("Selecione uma opção", opcoes)
+
+def main():
+    st.title("Análise exploratória de dados")
+    st.subheader("Datasets completos")
+    barra_Lateral()
+    carregar_dataset()
+
+if __name__ == "__main__":
+    main()
