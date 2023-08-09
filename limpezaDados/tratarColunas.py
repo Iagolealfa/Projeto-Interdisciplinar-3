ed6154bdfe0d6142e651e2a56d6b47bca53eb642
Caio Mizan <caiomizan1@gmail.com>
2023-08-09 12:44:07 -0300

    Tratamento de Race unspecified

--- limpezaDados/tratarColunas.py
+++ limpezaDados/tratarColunas.py
@@ -70,8 +70,10 @@ def imputar_race(file_path):
 
     mask = (df['Subjects_race'] == 'Race unspecified') & (df['Subjects_race_with_imputations'].notnull())
     df.loc[mask, 'Subjects_race'] = df.loc[mask, 'Subjects_race_with_imputations']
+    df.loc[mask, 'imputation_control'] = True  
 
     df.to_csv(file_path, index=False)
+
 
 
 def drop_colunas(file_path,name_coluna):
@@ -80,19 +82,42 @@ def drop_colunas(file_path,name_coluna):
     df = df.drop(columns=colunas_para_apagar)
 
     df.to_csv(file_path, index=False)
+
 def coluna_others(file_path,name_coluna):
     df = pd.read_csv(file_path)
     df[name_coluna] = df[name_coluna].apply(lambda x: x if x in ['Gunshot', 'Vehicle', 'Tasered'] else 'Others')
     df[name_coluna].to_csv("..\\data\\fatal_encounters_tratado.csv", index=False)
+
 def add_coluna(file_path,file_path_new,name_coluna):
     df1 = pd.read_csv(file_path)
     df2= pd.read_csv(file_path_new)
     df2[name_coluna] = df1[name_coluna]
     df2.to_csv("..\\data\\fatal_encounters_tratado.csv", index=False)    
+
+def add_imputation_control_column(file_path):
+    df = pd.read_csv(file_path)
+    df["imputation_control"] = False
+    df.to_csv(file_path, index=False)
+
+def replace_unspecified_race_with_mode(file_path):
+    df = pd.read_csv(file_path)
+    mode_value = df['Subjects_race'].mode().iloc[0]
+    df.loc[df['Subjects_race'] == 'Race unspecified', 'Subjects_race'] = mode_value
+    df.to_csv(file_path, index=False)
+
+
+
 def main():
     df1 = pd.read_csv(file_path)
     df2=pd.read_csv(file_path_new)
-    add_coluna(file_path,file_path_new,'Subjects_gender')
+    
+    #add_coluna(file_path,file_path_new,'Cause_of_death') Comentado pois a coluna ja foi adcionada 
+    #add_coluna(file_path,file_path_new,'Age') Comentado pois a coluna ja foi adcionada 
+    #add_coluna(file_path,file_path_new,'Subjects_gender') Comentado pois a coluna ja foi adcionada 
+    #add_imputation_control_column(file_path) Comentado pois a coluna ja foi adcionada com o valor nulo
+    #imputar_race(file_path) Comentado pois ja foi substituido os Race unspecified por Subjects_race_with_imputations quando possivel
+    #replace_unspecified_race_with_mode(file_path) Comentado pois já foi substituido os Race unspecified restantes pela moda
+
     st.write(df1['Subjects_gender'].value_counts())
 
     st.write(df1.columns)
