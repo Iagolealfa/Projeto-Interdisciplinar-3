f0267de26a809b4b049c8b4e3a8d793bff6f5e02
Iago Leal <70781193+Iagolealfa@users.noreply.github.com>
2023-08-27 23:20:52 -0300

    Ajustes na page dataall

--- pages/dataAll.py
+++ pages/dataAll.py
@@ -3,17 +3,8 @@ import pandas as pd
 def carregar_dataset():
     
     with st.expander('fatal_encounters_dot_org'):
-        fatal_encounters_dot_org = pd.read_csv('data\\fatal_encounters_dot_org.csv')
+        fatal_encounters_dot_org = pd.read_csv('data/fatal_encounters_tratado.csv')
         st.write(fatal_encounters_dot_org)
-    with st.expander('PoliceKillings'):
-        dataPoliceKillings = pd.read_csv("data\police_killings_MPV.csv")
-        st.write(dataPoliceKillings)
-    with st.expander('ShareRaceByCity'):
-        dataShareRaceByCity= pd.read_csv("data\ShareRaceByCity.csv")
-        st.write(dataShareRaceByCity)
-    with st.expander('ShootingsWash'):
-        shootingsWash = pd.read_csv("data\shootings_wash_post.csv")
-        st.write(shootingsWash)
 
 def cabecalho():
     st.title("Data Bucket")
