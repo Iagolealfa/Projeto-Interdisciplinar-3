793f2c98b33a54ffcbaae12bc7367cd418877463
AntonioNeto0 <antonioaraujo99@gmail.com>
2023-08-27 23:20:48 -0300

    Ajustando a home

--- home.py
+++ home.py
@@ -6,3 +6,16 @@ st.set_page_config(
         'About': "TESTE DO ABOUT"
     }
 )
+
+st.title("Análise de padrões de letalidade policial nos Estados Unidos")
+st.text('''Este projeto tem como objetivo analisar uma base de dados com informações de 
+encontro fatais com a policia nos estados unidos, utilizando mineração de dados
+e aprendizado de máquina''')
+st.text('''Projeto desenvolvido para disciplina Projeto Interdisciplinar 3 do curso de Sistema
+de Informação da Universidade Federal Rural de Pernambuco''')
+st.text('''Grupo:
+- Antonio Neto
+- Caio Mizan
+- Iago Leal
+''')
+st.text("Recife, 2023")
